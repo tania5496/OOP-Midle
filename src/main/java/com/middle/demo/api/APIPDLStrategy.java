f01cf76a6a4b4082448fba440785ab54e3178afb
sofiiafolv <92806105+sofiiafolv@users.noreply.github.com>
2022-12-03 18:09:10 +0200

    Update APIPDLStrategy.java

--- src/main/java/com/middle/demo/api/APIPDLStrategy.java
+++ src/main/java/com/middle/demo/api/APIPDLStrategy.java
@@ -19,7 +19,7 @@ public class APIPDLStrategy implements API{
                 .uri(URI.create(String.format("https://api.peopledatalabs.com/v5/company/enrich?website=%s&pretty=false", domain)))
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .header("X-API-Key", "0e6d1e2291ce067def630829f587c149c23a30b6fcab63626f5e4a99aa4e8619")
+                .header("X-API-Key", "e6695d7c8c570225e915388b990380b7e0de14fef85658bd73547fe1cede1ded")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
